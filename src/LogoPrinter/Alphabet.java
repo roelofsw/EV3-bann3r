6bf376f6af13c3955cf7d887e0cda24d60b4964d
Roelof Swanepoel <roelof.swanepoel@gmail.com>
2015-10-19 15:34:03 +1000

    Added initial code to move pen left and right

--- src/LogoPrinter/Alphabet.java
+++ src/LogoPrinter/Alphabet.java
@@ -3,14 +3,16 @@ package LogoPrinter;
 import LogoPrinter.Printer;
 
 public class Alphabet {
-	static Printer p = new Printer(0, 50, Printer.UP, 0);
+	static Printer p = new Printer(0.0, 6.0, Printer.UP, 0.0);
 	
 	public static void main(String [ ] args)
 	{
-		if (p.LiftPen(Printer.UP) == 0) System.out.println("Pen moved UP");
+//		if (p.LiftPen(Printer.UP) == 0) System.out.println("Pen moved UP");
 		if (p.LiftPen(Printer.DOWN) == 0) System.out.println("Pen moved DOWN");
-		if (p.LiftPen(Printer.UP) == 0) System.out.println("Pen moved UP");
+//		if (p.LiftPen(Printer.UP) == 0) System.out.println("Pen moved UP");
 		
-		if (p.MovePen(20, Printer.LEFT) == 0) System.out.println("Pen moved 20 left");
+		if (p.MovePen(1, Printer.DIR_LEFT) == 0) System.out.println("Pen moved 1 left");
+		if (p.MovePen(2, Printer.DIR_RIGHT) == 0) System.out.println("Pen moved 2 right");
+		if (p.MovePen(2, Printer.DIR_LEFT) == 0) System.out.println("Pen moved 2 left");
 	}
 }
--- src/LogoPrinter/Printer.java
+++ src/LogoPrinter/Printer.java
@@ -1,5 +1,8 @@
 package LogoPrinter;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import lejos.hardware.motor.EV3LargeRegulatedMotor;
 import lejos.hardware.motor.EV3MediumRegulatedMotor;
 import lejos.hardware.port.MotorPort;
@@ -8,25 +11,82 @@ import lejos.robotics.RegulatedMotor;
 public class Printer {
 	static final boolean UP = true;
 	static final boolean DOWN = false;
-	static final int LEFT = 1;
-	static final int RIGHT = 2;
+	static final int DIR_LEFT = 1;
+	static final int DIR_RIGHT = 2;
+	static final double MAX_LEFT = 0.0;
+	static final double MAX_RIGHT = 6.0;
+	static final int MAX_ANGLE = 476;
 
-	int leftBorder;
-	int rightBorder;
+	private double leftBorder;
+	private double rightBorder;
 	
-	boolean penStatus;
-	int penPosition;
+	private boolean penStatus;
+	private double penPosition;
+	private int penAngle;
+	private double[] positionTable = new double[MAX_ANGLE];
 	
 	static RegulatedMotor pen = new EV3MediumRegulatedMotor(MotorPort.A);
 	static RegulatedMotor lifter = new EV3LargeRegulatedMotor(MotorPort.B);
 	static RegulatedMotor rollers = new EV3LargeRegulatedMotor(MotorPort.C);
 	
-	public Printer(int leftBorder, int rightBorder, boolean penStatus, int penPosition)
+	private void BuildPenPositionLookupTable()
 	{
-		this.leftBorder = leftBorder;
-		this.rightBorder = rightBorder;
+		int smallAngle;
+		double largeAngle;
+		double r = 3.16;
+		
+		for (smallAngle=0; smallAngle < MAX_ANGLE; smallAngle++)
+		{
+			largeAngle = smallAngle/3.0;
+			double temp1 = r * Math.cos(largeAngle);
+			double temp2 = Math.pow(((r * Math.sin(largeAngle)) - 1),2);
+			double temp3 = Math.sqrt(25-temp2);
+			positionTable[smallAngle] = temp1 + temp3;
+		}
+	}
+	
+	private int FindAngleFromTable(double penPosition)
+	{
+		int loop;
+		int returnVal = MAX_ANGLE + 1;
+		
+		if ((penPosition >= this.MAX_LEFT) && (penPosition <= this.MAX_RIGHT))
+		{
+			for (loop = 0; loop < MAX_ANGLE; loop++)
+			{
+				if (this.positionTable[loop] >= (this.positionTable[0] - penPosition))
+				{
+					returnVal = loop;
+				}
+			}
+		}
+		return returnVal;
+	}
+	
+	public Printer(double leftBorder, double rightBorder, boolean penStatus, double penPosition)
+	{
+		if (leftBorder >= MAX_LEFT)
+		{
+			this.leftBorder = leftBorder;
+		}
+		else
+		{
+			this.leftBorder = MAX_LEFT;
+		}
+		
+		if (rightBorder <= MAX_RIGHT)
+		{
+			this.rightBorder = rightBorder;
+		}
+		else
+		{
+			this.rightBorder = MAX_RIGHT;
+		}
+
 		this.penStatus = penStatus;
 		this.penPosition = penPosition;
+		this.BuildPenPositionLookupTable();
+		this.penAngle = this.FindAngleFromTable(penPosition);
 	}
 
 	public int LiftPen(boolean UPDOWN)
@@ -60,18 +120,24 @@ public class Printer {
 	{
 		int returnVal = 1;
 		
-		if (direction == LEFT)
+		if (direction == DIR_LEFT)
 		{
 			if ((penPosition - distance) >= leftBorder)
 			{
 				// Move pen left
+				int targetAngle = this.FindAngleFromTable(penPosition - distance);
+				pen.rotateTo(penAngle - targetAngle);
+				returnVal = 0;
 			}
 		}
-		else if (direction == RIGHT)
+		else if (direction == DIR_RIGHT)
 		{
 			if ((penPosition + distance) <= rightBorder)
 			{
 				// Move pen right
+				int targetAngle = this.FindAngleFromTable(penPosition + distance);
+				pen.rotateTo(targetAngle - penAngle);
+				returnVal = 0;
 			}
 		}
 
